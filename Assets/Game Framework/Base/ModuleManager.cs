ad2a0768d5cf82c7fa9a8f9618fc5204c7975c92
cookapps-grkim <grkim@cookapps.com>
2020-01-31 19:03:16 +0900

    모듈 매니저 작성 중 (레퍼런스풀 같은 베이스 작업이 역시 먼저 필요함)

---
+++ Assets/Game Framework/Base/ModuleManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameFramework
+{
+    public static class ModuleManager
+    {
+        private static readonly GameFrameworkLinkedList<GameFrameworkModule> gameFrameworkModules = new GameFrameworkLinkedList<GameFrameworkModule>();
+
+        public static void Update(float elapseSeconds, float realElapseSeconds)
+        {
+            foreach(var module in gameFrameworkModules)
+            {
+                module.Update(elapseSeconds, realElapseSeconds);
+            }
+        }
+
+        public static void Shutdown()
+        {
+            for(var current = gameFrameworkModules.Last; current != null; current = current.Previous)
+            {
+                current.Value.Shutdown();
+            }
+
+            gameFrameworkModules.Clear();
+            //ReferencePool.ClearAll();            
+        }
+    }
+}
